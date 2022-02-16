1263591f46026a83b133ae57721b1515fa3adbff
Vitória <gitgitt0@gmail.com>
2022-02-16 16:20:53 -0300

    Initial commit

---
+++ Desafio aula 1/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Desafio_aula_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblOperacao = new System.Windows.Forms.Label();
+            this.txtOperacao = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(121, 200);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 0;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(262, 200);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtNumero1.Location = new System.Drawing.Point(202, 42);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero1.TabIndex = 2;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(202, 109);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero2.TabIndex = 3;
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Location = new System.Drawing.Point(136, 45);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(60, 15);
+            this.lblNumero1.TabIndex = 4;
+            this.lblNumero1.Text = "Número 1";
+            this.lblNumero1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Location = new System.Drawing.Point(136, 112);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(60, 15);
+            this.lblNumero2.TabIndex = 5;
+            this.lblNumero2.Text = "Número 2";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(202, 149);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 23);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(136, 152);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(59, 15);
+            this.lblResultado.TabIndex = 7;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblOperacao
+            // 
+            this.lblOperacao.AutoSize = true;
+            this.lblOperacao.Location = new System.Drawing.Point(70, 78);
+            this.lblOperacao.Name = "lblOperacao";
+            this.lblOperacao.Size = new System.Drawing.Size(126, 15);
+            this.lblOperacao.TabIndex = 9;
+            this.lblOperacao.Text = "Operação (+, -, *, /, %)";
+            // 
+            // txtOperacao
+            // 
+            this.txtOperacao.Location = new System.Drawing.Point(202, 75);
+            this.txtOperacao.Name = "txtOperacao";
+            this.txtOperacao.Size = new System.Drawing.Size(100, 23);
+            this.txtOperacao.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOperacao);
+            this.Controls.Add(this.txtOperacao);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnCancelar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblOperacao;
+        private System.Windows.Forms.TextBox txtOperacao;
+    }
+}
+
---
+++ Desafio aula 1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_aula_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1, n2, soma, sub, mult, div;
+            if (txtNumero1.Text == string.Empty && txtNumero2.Text == string.Empty && txtOperacao.Text == string.Empty)
+            {
+                MessageBox.Show("As caixas estão em branco");
+            }
+            else if (txtNumero1.Text == string.Empty)
+            {
+                MessageBox.Show("Texto em branco na 1 caixa");
+            }
+            else if (txtNumero2.Text == string.Empty)
+            {
+                MessageBox.Show("Texto em branco na 2 caixa");
+            }
+            else if (txtOperacao.Text == string.Empty)
+            {
+                MessageBox.Show("Qual operação você gostaria de realizar?");
+            }
+            else
+            {
+                if (txtOperacao.Text == "+")
+                {
+                    n1 = Convert.ToDouble(txtNumero1.Text);
+                    n2 = Convert.ToDouble(txtNumero2.Text);
+                    soma = n1 + n2;
+                    txtResultado.Text = Convert.ToString(soma);
+                }
+                else if (txtOperacao.Text == "-")
+                {
+                    n1 = Convert.ToDouble(txtNumero1.Text);
+                    n2 = Convert.ToDouble(txtNumero2.Text);
+                    sub = n1 - n2;
+                    txtResultado.Text = Convert.ToString(sub);
+                }
+                else if (txtOperacao.Text == "*" || txtOperacao.Text == "x" || txtOperacao.Text == "X" || txtOperacao.Text == ".")
+                {
+                    n1 = Convert.ToDouble(txtNumero1.Text);
+                    n2 = Convert.ToDouble(txtNumero2.Text);
+                    mult = n1 * n2;
+                    txtResultado.Text = Convert.ToString(mult);
+                }
+                else if (txtOperacao.Text == "/")
+                {
+                    if (txtNumero2.Text == "0")
+                    {
+                        MessageBox.Show("Não existe divisão por zero");
+                        txtNumero2.Text = string.Empty;
+                    }
+                    else
+                    {
+                        n1 = Convert.ToDouble(txtNumero1.Text);
+                        n2 = Convert.ToDouble(txtNumero2.Text);
+                        div = n1 / n2;
+                        txtResultado.Text = Convert.ToString(div);
+                    }
+                }
+                else if (txtOperacao.Text == "%")
+                {
+                    int nu1, nu2, mod;
+                    nu1 = Convert.ToInt16(txtNumero1.Text);
+                    nu2 = Convert.ToInt16(txtNumero2.Text);
+                    mod = nu1 % nu2;
+                    txtResultado.Text = Convert.ToString(mod);
+                }
+                else
+                {
+                    MessageBox.Show("Digite uma operação válida");
+                    txtOperacao.Focus();
+                    txtOperacao.Text = string.Empty;
+                }
+            }
+        }
+    
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = string.Empty;
+            txtNumero2.Text = string.Empty;
+            txtResultado.Text = string.Empty;
+            txtNumero1.Focus();
+        }
+    }
+}
